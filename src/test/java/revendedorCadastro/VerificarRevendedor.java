c36fa88c41a9b792b30570a33c9bd3496013d00e
Bruno Graciano Alves Carneiro <bruno.graciano@targetsoftware.com.br>
2018-03-27 16:15:31 -0300

    commit

--- src/test/java/revendedorCadastro/VerificarRevendedor.java
+++ src/test/java/revendedorCadastro/VerificarRevendedor.java
@@ -5,23 +5,22 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import junitparams.FileParameters;
-import junitparams.JUnitParamsRunner;
 import junitparams.mappers.CsvWithHeaderMapper;
-
 import static org.junit.Assert.assertEquals;
 
-import java.lang.Thread.*;
 
-@RunWith(JUnitParamsRunner.class)
+
 public class VerificarRevendedor {
+	
+	private WebDriver driver;
+	private DSL dsl;
 	
 	@Test
 	@Ignore
 	public void verificaRevendedorNaoCadastradoClickPesquisa() throws InterruptedException {
 		
-		System.setProperty("webdriver.gecko.driver", "/home/bcarneiro/SeleniumDriver/geckodriver");
+		//System.setProperty("webdriver.gecko.driver", "/home/bcarneiro/SeleniumDriver/geckodriver");
 		WebDriver driver = new FirefoxDriver();
 		driver.navigate().to("http://server.target1.com.br:9020/landing-page.html");
 		Thread.sleep(1000);
@@ -31,22 +30,32 @@ public class VerificarRevendedor {
 		assertEquals("Cadastro de Revendedor | Promoção Baterias Moura", driver.getTitle());
 	}
 	
+	
 	@Test
-	@FileParameters(value = "src/test/resource/massa_de_dados.csv", 
-				mapper = CsvWithHeaderMapper.class)
-	public void cadastrandoRevendedor(String fantasia, String razao_social, int cadastro_cep, int cadastro_numero, int cadastro_complemento, String email, int telefone, String distribuidor) throws InterruptedException {
+	@FileParameters(value = "src/test/resources/massa_de_dados.csv",
+		mapper = CsvWithHeaderMapper.class)
+	public void cadastrandoRevendedor() throws InterruptedException {
 		WebDriver driver = new FirefoxDriver();
 		driver.navigate().to("http://server.target1.com.br:9020/revendedor-cadastro.html");
-		driver.findElement(By.id(fantasia)).sendKeys(fantasia);
+		dsl.escrever("campocnpj", "03926599000187");
+		Thread.sleep(500);
+		dsl.clicarBotaoCass("btnBuscaRev");
+		dsl.escrever("fantasia", "Enzo e Pietro Telecom ME");
 		
-		/*driver.navigate().to("http://server.target1.com.br:9020/revendedor-cadastro.html");
-		Thread.sleep(1000);
-		driver.findElement(By.id("campocnpj")).sendKeys("03926599000187");
-		driver.findElement(By.className("btnBuscaRev")).click();
-		driver.findElement(By.id("fantasia")).sendKeys("Enzo e Pietro Telecom ME");
+		//driver.findElement(By.id("campocnpj")).sendKeys("03926599000187");		
+		//driver.findElement(By.className("btnBuscaRev")).click();
+		//driver.findElement(By.id("fantasia")).sendKeys("Enzo e Pietro Telecom ME");
 		driver.findElement(By.id("razao_social")).sendKeys("Enzo e Pietro Telecom ME");
-		driver.findElement(By.id("razao_social")).sendKeys("Enzo e Pietro Telecom ME");
-		*/
+		driver.findElement(By.id("cadastro_cep")).sendKeys("18050600");
+		driver.findElement(By.id("cadastro_numero")).sendKeys("1014");
+		driver.findElement(By.id("email")).sendKeys("gardenal@gardenal.com.br");
+		driver.findElement(By.id("telefone")).sendKeys("112458-5587");
+		//dsl.selecionarCombo("9", valor);
+		
+		
+		
+		
+		
 		
 	}
 
