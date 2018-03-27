e54d7f2bdc6f5f5153242dfb9abeb82ca67ed14c
bcarneiro <bcarneiro@Bruno-Linux>
2018-03-27 01:10:19 -0300

    commit

--- src/test/java/revendedorCadastro/VerificarRevendedor.java
+++ src/test/java/revendedorCadastro/VerificarRevendedor.java
@@ -1,16 +1,24 @@
 package revendedorCadastro;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.CsvWithHeaderMapper;
 
 import static org.junit.Assert.assertEquals;
 
 import java.lang.Thread.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class VerificarRevendedor {
 	
 	@Test
+	@Ignore
 	public void verificaRevendedorNaoCadastradoClickPesquisa() throws InterruptedException {
 		
 		System.setProperty("webdriver.gecko.driver", "/home/bcarneiro/SeleniumDriver/geckodriver");
@@ -24,7 +32,21 @@ public class VerificarRevendedor {
 	}
 	
 	@Test
-	public void cadastrandoRevendedor() {
+	@FileParameters(value = "src/test/resource/massa_de_dados.csv", 
+				mapper = CsvWithHeaderMapper.class)
+	public void cadastrandoRevendedor(String fantasia, String razao_social, int cadastro_cep, int cadastro_numero, int cadastro_complemento, String email, int telefone, String distribuidor) throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.navigate().to("http://server.target1.com.br:9020/revendedor-cadastro.html");
+		driver.findElement(By.id(fantasia)).sendKeys(fantasia);
+		
+		/*driver.navigate().to("http://server.target1.com.br:9020/revendedor-cadastro.html");
+		Thread.sleep(1000);
+		driver.findElement(By.id("campocnpj")).sendKeys("03926599000187");
+		driver.findElement(By.className("btnBuscaRev")).click();
+		driver.findElement(By.id("fantasia")).sendKeys("Enzo e Pietro Telecom ME");
+		driver.findElement(By.id("razao_social")).sendKeys("Enzo e Pietro Telecom ME");
+		driver.findElement(By.id("razao_social")).sendKeys("Enzo e Pietro Telecom ME");
+		*/
 		
 	}
 
